ae778d92c1fbc40f45c4214b4f904b6e3904c2da
carlos lopez <carlos25u007@gmail.com>
2021-11-15 13:30:17 -0400

    Creando Entidad Estudiante

---
+++ Entidades/Estudiantes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncripcionesWPF.Entidades
+{
+    class Estudiantes
+    {
+        [Key]
+        public int EstudianteId { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; } = DateTime.Now;
+        public String Direccion { get; set; }
+        public String Telefono { get; set; }
+        public String Correo { get; set; }
+
+    }
+}
