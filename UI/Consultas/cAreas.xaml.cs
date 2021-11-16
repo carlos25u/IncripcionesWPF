d1823cb01066573633b477f4c6f7694b575867f5
Pedro Luis Solorin Zorilla <pedro_solorin@ucne.edu.do>
2021-11-16 17:52:23 -0400

    Haciendo Consultas de Areas

---
+++ UI/Consultas/cAreas.xaml.cs
@@ -0,0 +1,55 @@
+﻿using IncripcionesWPF.BLL;
+using IncripcionesWPF.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IncripcionesWPF.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for cAreas.xaml
+    /// </summary>
+    public partial class cAreas : Window
+    {
+        public cAreas()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Areas>();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = AreasBLL.GetList(e => e.AreaId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+
+                    case 1:
+                        listado = AreasBLL.GetList(e => e.Descripcion.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                }
+            }
+            else
+            {
+                listado = AreasBLL.GetList(c => true);
+            }
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+        }
+    }
+}
