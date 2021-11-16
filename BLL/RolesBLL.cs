df4838260b7c596bc214fcb96046d703845ae6b6
carlos lopez <carlos25u007@gmail.com>
2021-11-16 09:47:33 -0400

    Haciendo Entidad Roles y RolesBLL

---
+++ BLL/RolesBLL.cs
@@ -0,0 +1,35 @@
+﻿using IncripcionesWPF.DAL;
+using IncripcionesWPF.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncripcionesWPF.BLL
+{
+    class RolesBLL
+    {
+        public static List<Roles> GetRoles()
+        {
+            var lista = new List<Roles>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Roles.ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -12,11 +12,41 @@ namespace IncripcionesWPF.DAL
     {
         public DbSet<Estudiantes> Estudiantes { get; set; }
         public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Roles> Roles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(@"Data Source = DATA\Incripciones.db");
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuarios>().HasData(new Usuarios
+            {
+                UsuariosId = 1,
+                Nombre = "Carlos",
+                Apellido = "Solorin",
+                NombreUsuario = "Admin",
+                Clave = "1234",
+                FechaCreacion = DateTime.Now
+
+            }
+            );
+
+            modelBuilder.Entity<Roles>().HasData(new Roles
+            {
+              RolId = 1,
+              Descripcion = "Admin"
+            }
+            );
+
+            modelBuilder.Entity<Roles>().HasData(new Roles
+            {
+                RolId = 2,
+                Descripcion = "Secretaria"
+            }
+           );
         }
     }
 }
