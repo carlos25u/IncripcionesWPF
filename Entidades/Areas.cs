fc7c80f5e27da95f219c6e367d48d14c085512f6
Pedro Luis Solorin Zorilla <pedro_solorin@ucne.edu.do>
2021-11-16 10:04:03 -0400

    Creando entidad Areas, Materias, Y CursosDetalle

---
+++ Entidades/Areas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncripcionesWPF.Entidades
+{
+    public class Areas
+    {
+        [Key]
+        public int AreaId { get; set; }
+        public String Descripcion { get; set; }
+    }
+}
---
+++ Entidades/Materias.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncripcionesWPF.Entidades
+{
+    public class Materias
+    {
+        [Key]
+        public int MateriaId { get; set; }
+        public String CodigoMateria { get; set; }
+        public String Nombre { get; set; }
+    }
+}
