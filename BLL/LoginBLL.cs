a9f6f7d41a06a33a1c98d0f863daa414b13d77d5
carlos lopez <carlos25u007@gmail.com>
2021-11-19 09:00:22 -0400

    Haciendo LoginBLL

---
+++ BLL/LoginBLL.cs
@@ -0,0 +1,42 @@
+﻿using IncripcionesWPF.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncripcionesWPF.BLL
+{
+    class LoginBLL
+    {
+        public static bool Validar(string nombreusuario, string clave)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var validar = from usuario in contexto.Usuarios
+                              where usuario.NombreUsuario == nombreusuario
+                              && usuario.Clave == clave
+                              select usuario;
+
+                if (validar.Count() > 0)
+                    paso = true;
+                else
+                    paso = false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+    }
+}
